38e6130563d85f7178e73c19b50045ff09bfaf2f
Ermmy77 <malamalamarick@gmail.com>
2025-03-27 11:10:58 +1300

    Created the database schema

---
+++ database/schema.sql
@@ -0,0 +1,37 @@
+if exists drop table user;
+if exists drop table supermarket;
+if exists drop table product;
+if exists drop table category;
+
+
+create table User(
+    user_id smallint not null,
+    first_name varchar(250) not null,
+    last_name varchar(250) not null,
+    password varchar(250) not null,
+    email_address varchar(250) not null,
+    home_address varchar(250),
+    constraint user_pk primary key (user_id)
+);
+
+create table Supermarket(
+    supermarket_name varchar(250) not null,
+    address varchar(250) not null,
+    constriant supermarket_pk primary key (supermarket_name)
+);
+
+create table Category(
+    category_name varchar(250) not null,
+    constraint category_pk primary key (category_name)
+);
+
+create table Product(
+    product_id smallint not null,
+    category_name varchar(250) not null,
+    name varchar(250) not null,
+    price smallint not null,
+    image_url varchar(250) not null,
+    supermarket_name varchar(250) not null,
+    constraint product_pk primary key (product_id),
+    constraint product_fk foreign key (catgory_name) references Category(category_name)
+);
