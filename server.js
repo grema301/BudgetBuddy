0483af9ef7d466bf3c721813f5a8cadec061ab96
Nkanyiso <ndlnk218@student.otago.ac.nz>
2025-04-09 16:14:49 +1200

    including a .gitignore file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'web')));
 const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'BudgetBuddy', 
+    password: '', 
     database: 'BudgetBuddy'
 };
 
