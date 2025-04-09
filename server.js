b6b78bd252868a76a70a495c8d05df95ea50b708
Nkanyiso <ndlnk218@student.otago.ac.nz>
2025-04-09 16:28:50 +1200

    testing ignore function

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'web')));
 const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'anotherNewPassword', 
+    password: '', 
     database: 'BudgetBuddy'
 };
 
