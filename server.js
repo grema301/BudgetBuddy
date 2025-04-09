aa2d4230ed4e65d62098892eeeba10eb042d4807
Nkanyiso <ndlnk218@student.otago.ac.nz>
2025-04-09 16:27:26 +1200

    updating gitignore

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'web')));
 const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'newPassword', 
+    password: 'anotherNewPassword', 
     database: 'BudgetBuddy'
 };
 
