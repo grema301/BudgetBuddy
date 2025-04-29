9e8135842370bcbb0b813f2bc685ce8104c60e57
Nkanyiso <ndlnk218@student.otago.ac.nz>
2025-04-29 22:02:48 +1200

    schema update to update tables in the database to match adjusted schema

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,7 @@ drop table if exists Product;
 drop table if exists Category;
 
 
-create table User(
+create or replace table User(
     user_id INT not null AUTO_INCREMENT,
     first_name varchar(250) not null,
     last_name varchar(250) not null,
@@ -14,18 +14,18 @@ create table User(
     constraint user_pk primary key (user_id)
 );
 
-create table Supermarket(
+create or replace table Supermarket(
     supermarket_name varchar(250) not null,
     address varchar(250) not null,
     constraint supermarket_pk primary key (supermarket_name)
 );
 
-create table Category(
+create or replace table Category(
     category_name varchar(250) not null,
     constraint category_pk primary key (category_name)
 );
 
-create table Product(
+create or replace table Product(
     product_id INT not null AUTO_INCREMENT,
     category_name varchar(250),
     product_name varchar(250) not null unique,
@@ -34,7 +34,7 @@ create table Product(
     constraint product_pk primary key (product_id)
 );
 
-CREATE TABLE Price (
+CREATE or replace TABLE Price (
     product_id INT NOT NULL,
     supermarket_name varchar(250) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
